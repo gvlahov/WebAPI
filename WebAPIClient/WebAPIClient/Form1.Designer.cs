a9ca049d335245c1de8aa21a068f6f039c1ec755
gvlahov <grigor.vlahov@gmail.com>
2017-04-04 18:47:44 +0300

    Implemented

--- WebAPIClient/WebAPIClient/Form1.Designer.cs
+++ WebAPIClient/WebAPIClient/Form1.Designer.cs
@@ -28,12 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControlPages = new System.Windows.Forms.TabControl();
+            this.tabPageRequest = new System.Windows.Forms.TabPage();
+            this.textBoxRequest = new System.Windows.Forms.TextBox();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.buttonCall = new System.Windows.Forms.Button();
+            this.tabPageResponse = new System.Windows.Forms.TabPage();
+            this.textBoxResponse = new System.Windows.Forms.TextBox();
+            this.tabControlPages.SuspendLayout();
+            this.tabPageRequest.SuspendLayout();
+            this.tabPageResponse.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControlPages
+            // 
+            this.tabControlPages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControlPages.Controls.Add(this.tabPageRequest);
+            this.tabControlPages.Controls.Add(this.tabPageResponse);
+            this.tabControlPages.Location = new System.Drawing.Point(12, 12);
+            this.tabControlPages.Name = "tabControlPages";
+            this.tabControlPages.SelectedIndex = 0;
+            this.tabControlPages.Size = new System.Drawing.Size(760, 537);
+            this.tabControlPages.TabIndex = 0;
+            // 
+            // tabPageRequest
+            // 
+            this.tabPageRequest.Controls.Add(this.textBoxRequest);
+            this.tabPageRequest.Controls.Add(this.textBoxURL);
+            this.tabPageRequest.Controls.Add(this.buttonCall);
+            this.tabPageRequest.Location = new System.Drawing.Point(4, 22);
+            this.tabPageRequest.Name = "tabPageRequest";
+            this.tabPageRequest.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageRequest.Size = new System.Drawing.Size(752, 511);
+            this.tabPageRequest.TabIndex = 0;
+            this.tabPageRequest.Text = "Request";
+            this.tabPageRequest.UseVisualStyleBackColor = true;
+            // 
+            // textBoxRequest
+            // 
+            this.textBoxRequest.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxRequest.Location = new System.Drawing.Point(6, 35);
+            this.textBoxRequest.Multiline = true;
+            this.textBoxRequest.Name = "textBoxRequest";
+            this.textBoxRequest.Size = new System.Drawing.Size(740, 470);
+            this.textBoxRequest.TabIndex = 3;
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxURL.Location = new System.Drawing.Point(87, 8);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(659, 20);
+            this.textBoxURL.TabIndex = 2;
+            this.textBoxURL.Text = "http://localhost:58255/api/orders";
+            // 
+            // buttonCall
+            // 
+            this.buttonCall.Location = new System.Drawing.Point(6, 6);
+            this.buttonCall.Name = "buttonCall";
+            this.buttonCall.Size = new System.Drawing.Size(75, 23);
+            this.buttonCall.TabIndex = 1;
+            this.buttonCall.Text = "Call";
+            this.buttonCall.UseVisualStyleBackColor = true;
+            this.buttonCall.Click += new System.EventHandler(this.buttonCall_Click);
+            // 
+            // tabPageResponse
+            // 
+            this.tabPageResponse.Controls.Add(this.textBoxResponse);
+            this.tabPageResponse.Location = new System.Drawing.Point(4, 22);
+            this.tabPageResponse.Name = "tabPageResponse";
+            this.tabPageResponse.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageResponse.Size = new System.Drawing.Size(752, 511);
+            this.tabPageResponse.TabIndex = 1;
+            this.tabPageResponse.Text = "Response";
+            this.tabPageResponse.UseVisualStyleBackColor = true;
+            // 
+            // textBoxResponse
+            // 
+            this.textBoxResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxResponse.Location = new System.Drawing.Point(6, 6);
+            this.textBoxResponse.Multiline = true;
+            this.textBoxResponse.Name = "textBoxResponse";
+            this.textBoxResponse.Size = new System.Drawing.Size(740, 499);
+            this.textBoxResponse.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.tabControlPages);
+            this.Name = "Form1";
+            this.Text = "Web API Client";
+            this.tabControlPages.ResumeLayout(false);
+            this.tabPageRequest.ResumeLayout(false);
+            this.tabPageRequest.PerformLayout();
+            this.tabPageResponse.ResumeLayout(false);
+            this.tabPageResponse.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControlPages;
+        private System.Windows.Forms.TabPage tabPageRequest;
+        private System.Windows.Forms.TabPage tabPageResponse;
+        private System.Windows.Forms.Button buttonCall;
+        private System.Windows.Forms.TextBox textBoxRequest;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.TextBox textBoxResponse;
+
     }
 }
 
--- WebAPIClient/WebAPIClient/Form1.cs
+++ WebAPIClient/WebAPIClient/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,80 @@ namespace WebAPIClient
         public Form1()
         {
             InitializeComponent();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("{");
+            stringBuilder.AppendLine("  \"apiToken\": \"2C8BF34D-7E97-BB65-2C3C-179BE9D6D4C4\",");
+            stringBuilder.AppendLine("  \"bcmContactInfo\": {");
+            stringBuilder.AppendLine("    \"firstName\": \"Име\",");
+            stringBuilder.AppendLine("    \"middleName\": \"Презиме\",");
+            stringBuilder.AppendLine("    \"lastName\": \"Фамилия\",");
+            stringBuilder.AppendLine("    \"phone\": \"359123456\",");
+            stringBuilder.AppendLine("    \"bcmContactId\": 123456,");
+            stringBuilder.AppendLine("    \"bcmLanguageId\": 2,");
+            stringBuilder.AppendLine("    \"email\": \"sales@varnadatacenter.com\",");
+            stringBuilder.AppendLine("    \"company\": \"Варна Дейта Център\",");
+            stringBuilder.AppendLine("    \"country\": \"България\",");
+            stringBuilder.AppendLine("    \"city\": \"Варна\",");
+            stringBuilder.AppendLine("    \"streetAddress\": \"ул. Н/михайловски 18\"");
+            stringBuilder.AppendLine("  },");
+            stringBuilder.AppendLine("  \"bcmEventInfo\": {");
+            stringBuilder.AppendLine("    \"operatorId\": \"D7A7FB71-AA17-4097-A6D5-CED720908D50\",");
+            stringBuilder.AppendLine("    \"eventHistoryId\": 123456,");
+            stringBuilder.AppendLine("    \"eventType\": 1");
+            stringBuilder.AppendLine("  },");
+            stringBuilder.AppendLine("  \"serviceInfo\": {");
+            stringBuilder.AppendLine("    \"brand\": \"Марка\",");
+            stringBuilder.AppendLine("    \"model\": \"Модел\",");
+            stringBuilder.AppendLine("    \"guarantee\": true,");
+            stringBuilder.AppendLine("    \"description\": \"Описание на повредата и др.\"");
+            stringBuilder.AppendLine("  }");
+            stringBuilder.AppendLine("}");
+
+            textBoxRequest.Text = stringBuilder.ToString();
+        }
+
+        private async void buttonCall_Click(object sender, EventArgs e)
+        {
+            buttonCall.Enabled = false;
+
+            textBoxResponse.Text = string.Empty;
+
+            string uri = textBoxURL.Text;
+            string jsonResponse = string.Empty;
+            string jsonRequest = textBoxRequest.Text;
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromSeconds(30);
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //Task<HttpResponseMessage> responseTask =  httpClient.PostAsync(uri, new StringContent(jsonRequest, Encoding.UTF8, "application/json"));
+
+            // await and process request
+            try
+            {
+                HttpResponseMessage response = await httpClient.PostAsync(uri, new StringContent(jsonRequest, Encoding.UTF8, "application/json"));
+                //HttpResponseMessage response = await responseTask;
+                response.EnsureSuccessStatusCode();    // throw if not a success code.
+                jsonResponse = await response.Content.ReadAsStringAsync();
+            }
+            //catch (HttpRequestException ex)
+            catch (Exception ex)
+            {
+                textBoxResponse.Text = ex.Message;
+            }
+            finally
+            {
+                httpClient.Dispose();
+            }
+
+            if (jsonResponse != string.Empty)
+            {
+                textBoxResponse.Text = jsonResponse;
+            }
+
+            tabControlPages.SelectedIndex = 1;
+
+            buttonCall.Enabled = true;
         }
     }
 }
