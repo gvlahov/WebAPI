e88f0ba6fc2f19a799eccf42a1e85747d0324ac3
gvlahov <grigor.vlahov@gmail.com>
2017-04-05 12:56:22 +0300

    Ready

---
+++ WebAPIServer/WebAPIServer/Controllers/OrdersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIServer.Models;
+
+namespace WebAPIServer.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        // POST api/orders
+        public Response Post([FromBody]Request request)
+        {
+            try
+            {
+                string test = request.apiToken.ToString()
+                    + request.bcmContactInfo.firstName
+                    + request.bcmContactInfo.middleName
+                    + request.bcmContactInfo.lastName
+                    ;
+
+                return new Response { success = true };
+            }
+            catch (Exception ex)
+            {
+                return new Response { success = false, message = ex.Message };
+            }
+        }
+    }
+}
---
+++ WebAPIServer/WebAPIServer/Models/Request.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIServer.Models
+{
+    public class Request
+    {
+        public Guid apiToken { get; set; }
+        public ContactInfo bcmContactInfo { get; set; }
+        public EventInfo bcmEventInfo { get; set; }
+        public ServiceInfo serviceInfo { get; set; }
+    }
+
+    public class ContactInfo
+    {
+        public string firstName { get; set; }
+        public string middleName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }
+        public int bcmContactId { get; set; }
+        public int bcmLanguageId { get; set; }
+        public string email { get; set; }
+        public string company { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+        public string streetAddress { get; set; }
+    }
+
+    public class EventInfo
+    {
+        public Guid operatorId { get; set; }
+        public int eventHistoryId { get; set; }
+        public int eventType { get; set; }
+    }
+
+    public class ServiceInfo
+    {
+        public string brand { get; set; }
+        public string model { get; set; }
+        public bool guarantee { get; set; }
+        public string description { get; set; }
+    }
+}
